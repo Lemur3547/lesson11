0ba7e7717d80a2890096ff326a4c320d4e6a35db
Эммануил Галиханов <emikgal2507@gmail.com>
2024-02-15 21:10:52 +0500

    add big first letter func and docstrings

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def caps_lock(text):
+    """
+    Функция делает все буквы заглавными
+    """
     return text.upper()
+
+
+def first_letter(text):
+    """
+    Функция делает заглавными первые буквы каждого слова
+    """
+    return text.title()
